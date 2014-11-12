fe71939773e3e70191dabcd9d1e77873cf1dc055
dingy007 <dingy007@gmail.com>
2014-11-13 00:59:25 +0800

    Creating functions.

--- boxsetup.sh
+++ boxsetup.sh
@@ -9,19 +9,25 @@
 program=""
 dropbox_ubuntu=""
 download_links_file="download_links"
+# Defining the different installers for different distribution
 ubuntu_autoinstaller="sudo apt-get install "
 fedora_autoinstaller="sudo yum install "
 debian_autoinstaller="sudo apt-get install "
+# Defining the different types of downloadable file formats
+# tar
+# tar.gz
+# tar.bz2
+# gzip
+# zip
+# rpm
+# deb
 
 int=0
 declare -a programs
+declare -a val
 
-echo "Starting the box setup script."
-echo "Enter the Linux Box type:"
-#read linuxDistro
-linuxDistro="ubunTU"
-
-#check if the input text file was provided as option param
+# Check if the input text file was provided as option param;
+# Else use the default file in the current location
 if [[ -z $1 ]]; 
 then
 	inputfile="program.txt"
@@ -29,30 +35,40 @@ else
 	inputfile=$1
 fi
 
-#read the input file
-#obtain the program names as 'programs' Array, size of array is 'int'
-while read -r line
-do
-	program=$line 
-	programs[ int ]=$line
-	int=$((int+1))
-done < "$inputfile"
+# Read the input file
+# Obtain the program names as 'programs' Array, size of array is 'int'
+function readProgramfile {
+	echo "--------------------------------------"
+	echo "	Reading the programs for installation from '$1'"
+	while read -r line
+	do
+		program=$line 
+		programs[ int ]=$line
+		int=$((int+1))
+	done < "$1" 
 
-echo "The following $int programs will be installed for $linuxDistro"
-echo ${programs[*])}
-#run a for loop through each of the programs
-for program in "${programs[@]}";
-do
-	echo "Program name: $program"
-done
+	echo "The following $int programs will be installed for $linuxDistro"
+	echo ${programs[*])}
+}
+
+# Function to print all of the required programs
+function printAllPrograms {
+	for program in "${programs[@]}";
+	do
+		echo "Program name: $program"
+	done
+}
 
 shopt -s nocasematch 
 
-#get links
-val=$(awk -F':=' '{print $2}' "$download_links_file");
+# Get links
+#val[0]=$(awk -F':=' '{print $2}' "$download_links_file");
 
-echo "************"
-echo "value of val: ${val}"
+function readDownloadLinksFile {
+	echo "--------------------------------------"
+	echo "	Reading the Download Links File"
+}
+
 function ubuntu_download {
 	echo "Installing programs for Ubuntu"
 	echo "wget $programs"
@@ -65,6 +81,23 @@ function fedora_download {
 function debian_download {
 	echo "Installing programs for Debian"
 }
+
+
+
+
+# No Function calls should be defined below this, to provide clarity.
+echo "STARTING THE SCRIPT..."
+# Get the Distribution type from the User
+echo "Enter the Linux Distribution type[ UBUNTU|RHEL|DEBIAN ] :"
+#read linuxDistro
+linuxDistro="ubunTU"
+echo "User entered: '$linuxDistro'"
+
+
+readProgramfile $inputfile
+printAllPrograms
+
+
 
 if [[ $linuxDistro == "ubuntu" ]];
 	then

918cedafdaec8f1622a50e83005acd3783e93916
dingy007 <dingy007@gmail.com>
2014-12-08 19:42:12 +0800

    Updated reading of downloads file

--- boxsetup.sh
+++ boxsetup.sh
@@ -24,7 +24,9 @@ debian_autoinstaller="sudo apt-get install "
 
 int=0
 declare -a programs
-declare -a val
+declare -a download_links
+declare -a ubuntu_download_links
+
 
 # Check if the input text file was provided as option param;
 # Else use the default file in the current location
@@ -42,18 +44,22 @@ function readProgramfile {
 	echo "	Reading the programs for installation from '$1'"
 	while read -r line
 	do
-		program=$line 
+		#program=$line 
 		programs[ int ]=$line
 		int=$((int+1))
 	done < "$1" 
 
 	echo "The following $int programs will be installed for $linuxDistro"
-	echo ${programs[*])}
+	for program in ${programs[@]}
+	do
+		#echo $entry
+		echo $program
+	done
 }
 
 # Function to print all of the required programs
 function printAllPrograms {
-	for program in "${programs[@]}";
+	for program in ${programs[@]}
 	do
 		echo "Program name: $program"
 	done
@@ -62,16 +68,57 @@ function printAllPrograms {
 shopt -s nocasematch 
 
 # Get links
-#val[0]=$(awk -F':=' '{print $2}' "$download_links_file");
+#val=$(awk -F':=' '{print $2}' "$download_links_file");
+#for eachVal in $(val[@])
+#do
+#	echo $eachVal
+#done
 
 function readDownloadLinksFile {
 	echo "--------------------------------------"
-	echo "	Reading the Download Links File"
+	echo "	Reading the download links file '$download_links_file'"
+	IFS=$'\n'
+	int=0
+	while read -r line
+	do
+		#download_links=$line 
+		download_links[ $int ]=$line
+		int=$((int+1))
+		
+	done < "$download_links_file" 
+
+	echo "The following $int programs will be installed for $linuxDistro"
+	
+	for downloadsEntry in ${download_links[@]}
+	do
+		echo $downloadsEntry
+	done
+	download_links[@]
 }
 
 function ubuntu_download {
 	echo "Installing programs for Ubuntu"
 	echo "wget $programs"
+}
+
+function get_ubuntu_downloads_list {
+	
+	counter=0
+	echo "Getting the list of programs for Ubuntu..."
+	for downloadsEntry in ${download_links[@]}
+	do
+		if [ downloadsEntry == "ubuntu" ]
+		then
+			ubuntu_download_links[ $counter ] = ${downloadsEntry}
+			counter=$(counter+1)
+			echo $counter
+		fi
+	done
+	for ubuntu_entry in $(ubuntu_download_links[@])
+	do
+		echo $ubuntu_entry
+	done
+	#echo "wget $programs"
 }
 
 function fedora_download {
@@ -95,11 +142,15 @@ echo "User entered: '$linuxDistro'"
 
 
 readProgramfile $inputfile
-printAllPrograms
+download_links=readDownloadLinksFile
+#printAllPrograms
+
 
 
 
 if [[ $linuxDistro == "ubuntu" ]];
 	then
-	ubuntu_download
+		get_ubuntu_downloads_list
+		ubuntu_download
 fi
+shopt -u nocasematch
